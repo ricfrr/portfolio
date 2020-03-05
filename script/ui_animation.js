fd2d82c751622c59039d9afc8e6a1887e26be8e8
ricfrr <riccardo.franceschini@live.i>
2020-03-05 21:46:56 +0100

    quick fix animation navbar

--- script/ui_animation.js
+++ script/ui_animation.js
@@ -9,29 +9,35 @@ function response() {
     time = 120
     
     
-    if (x.className === "m_navbar m_navbar_animation" || x.className === "m_navbar" || x.className =="m_navbar responsive t") {
+    if (x.className === "m_navbar m_navbar_animation" || x.className === "m_navbar" ) {
         x.className = "m_navbar responsive";
         x.animate([
-            {bottom: '-34%' },
-            {bottom: '0' }
-        ], { duration: time, fill:"forwards"})
+            {bottom: '-24%' },
+            {bottom: '0%' }
+        ], { duration: time, fill:'forwards'})
+        icon.animate([
+            { transform: 'rotate(0deg)' }, 
+            { transform: 'rotate(180deg)' } 
+        ], {duration:time, fill:"forwards"})
+
         icon.animate([
             { transform: 'rotate(0deg)' }, 
             { transform: 'rotate(180deg)' } 
         ], {duration:time, fill:"forwards"})
     } else {   
-        
-        x.className = "m_navbar responsive t";
+        var timer = setTimeout(function () {
+            x.className = "m_navbar";
+            }, time);
         x.animate([
             {bottom: '0%' },
-            {bottom: '-34%' }
-        ], { duration: time, fill:"forwards"})
+            {bottom: '-29%' }
+        ], { duration: time+10})
         icon.animate([
             { transform: 'rotate(180deg)' },
             { transform: 'rotate(270deg)' } ,
             { transform: 'rotate(360deg)' } 
         ], {duration:time, fill:"forwards"})
-
+        
       
     }
 }
